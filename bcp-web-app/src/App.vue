14f96affd4d3df2e74214d8fdfff31e4ce988440
atsushi <london.rightside@gmail.com>
2024-01-27 09:22:04 +0900

    アプリケーションレイアウトの導入。Vuetify導入の追加設定。

--- bcp-web-app/src/App.vue
+++ bcp-web-app/src/App.vue
@@ -1,7 +1,53 @@
 <script setup>
+import { ref } from "Vue";
 import LoginComponent from "./components/pages/LoginComponent.vue";
+
+/**
+ * ナビゲーションドロアーの表示状態。
+ * @type {Boolean}
+ */
+const isShowDrawer = ref(false);
+
+/**
+ * ログアウトする。
+ */
+const onLogout = () => {
+  console.log("onLogout()");
+};
 </script>
 
 <template>
-  <LoginComponent />
+  <v-app>
+    <!-- 画面上部のバー領域 -->
+    <v-app-bar>
+      <!-- メニューアイコン -->
+      <v-app-bar-nav-icon
+        @click.stop="isShowDrawer = !isShowDrawer"
+      ></v-app-bar-nav-icon>
+
+      <!-- タイトル -->
+      <v-app-bar-title>安否登録</v-app-bar-title>
+
+      <!-- スペーサー -->
+      <!-- <v-spacer></v-spacer> -->
+
+      <!-- ログインユーザ名 -->
+      <div class="mr-5">ログイン者</div>
+    </v-app-bar>
+
+    <!-- 画面左部のメニュー領域 -->
+    <v-navigation-drawer v-model="isShowDrawer" location="left" temporary>
+      <v-list>
+        <v-list-item title="メニュー"></v-list-item>
+        <v-divider />
+        <v-divider />
+        <v-list-item title="ログアウト" @click="onLogout"> </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- メイン領域 -->
+    <v-main class="d-flex align-start justify-center" style="min-height: 300px">
+      <LoginComponent />
+    </v-main>
+  </v-app>
 </template>
